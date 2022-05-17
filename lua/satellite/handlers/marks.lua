46568fb7e7dee569da267c9096aef0019cec070a
mohsen <smoka7@hotmail.com>
2022-05-17 18:31:55 +0430

    feat:update the bar when adding new marks

--- lua/satellite/handlers/marks.lua
+++ lua/satellite/handlers/marks.lua
@@ -2,24 +2,37 @@ local highlight = 'Normal'
 
 ---@type Handler
 local handler = {
-	name = 'marks',
+  name = 'marks',
 }
 
-function handler.init() end
+local function refresh()
+  require('satellite').refresh_bars()
+end
+
+function handler.init()
+  -- range over a-z
+  for char = 97, 122 do
+    local map = 'm' .. string.char(char)
+    vim.keymap.set({ 'n', 'v' }, map, function()
+      vim.schedule(refresh)
+      return map
+    end, { unique = true, expr = true })
+  end
+end
 
 function handler.update(bufnr)
-	local marks = {}
-	local buffer_marks = vim.fn.getmarklist(bufnr)
-	for _, mark in ipairs(buffer_marks) do
-		marks[#marks + 1] = {
-			-- [bufnum, lnum, col, off]
-			lnum = mark.pos[2] + 1,
-			-- first char of mark name is a single quote
-			symbol = string.sub(mark.mark, 2, 3),
-			highlight = highlight,
-		}
-	end
-	return marks
+  local marks = {}
+  local buffer_marks = vim.fn.getmarklist(bufnr)
+  for _, mark in ipairs(buffer_marks) do
+    marks[#marks + 1] = {
+      -- [bufnum, lnum, col, off]
+      lnum = mark.pos[2] + 1,
+      -- first char of mark name is a single quote
+      symbol = string.sub(mark.mark, 2, 3),
+      highlight = highlight,
+    }
+  end
+  return marks
 end
 
 require('satellite.handlers').register(handler)
