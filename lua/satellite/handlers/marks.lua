bce63277689eaea8b8f55a6724bbac3405d3dc15
mohsen <smoka7@hotmail.com>
2022-05-17 20:43:23 +0430

    feat:don't show builtin marks by default

--- lua/satellite/handlers/marks.lua
+++ lua/satellite/handlers/marks.lua
@@ -5,8 +5,21 @@ local handler = {
   name = 'marks',
 }
 
+local builtin_marks = { '\'.', '\'^', '\'\'', '\'"', '\'<', '\'>', '\'[', '\']' }
+
 local function refresh()
   require('satellite').refresh_bars()
+end
+
+---@param m string mark name
+---@return boolean
+local function its_a_builtin_mark(m)
+  for _, mark in pairs(builtin_marks) do
+    if mark == m then
+      return true
+    end
+  end
+  return false
 end
 
 function handler.init()
@@ -24,13 +37,15 @@ function handler.update(bufnr)
   local marks = {}
   local buffer_marks = vim.fn.getmarklist(bufnr)
   for _, mark in ipairs(buffer_marks) do
-    marks[#marks + 1] = {
-      -- [bufnum, lnum, col, off]
-      lnum = mark.pos[2] + 1,
-      -- first char of mark name is a single quote
-      symbol = string.sub(mark.mark, 2, 3),
-      highlight = highlight,
-    }
+    if not its_a_builtin_mark(mark.mark) then
+      marks[#marks + 1] = {
+        -- [bufnum, lnum, col, off]
+        lnum = mark.pos[2],
+        -- first char of mark name is a single quote
+        symbol = string.sub(mark.mark, 2, 3),
+        highlight = highlight,
+      }
+    end
   end
   return marks
 end
